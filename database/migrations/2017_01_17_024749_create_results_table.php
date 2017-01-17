2fe926cbda9f85cc3c204464ea2518e02577439b
AnhLeThiNgoc <le.thi.ngoc.anh@framgia.com>
2017-01-17 10:50:02 +0700

    Task #97873 [DB] Create DB
    update  migrate users table

---
+++ database/migrations/2017_01_17_024749_create_results_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('lesson_id');
+            $table->integer('word_id');
+            $table->integer('answer_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('results');
+    }
+}
